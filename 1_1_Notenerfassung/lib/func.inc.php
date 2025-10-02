5a3f6e9981602c51fd87cdc7e936463a726748da
FSchnabel2 <frauschnabel623@gmail.com>
2025-10-02 16:53:49 +0200

    Implementierung der anderen Validierungs-Funktionen

--- 1_1_Notenerfassung/lib/func.inc.php
+++ 1_1_Notenerfassung/lib/func.inc.php
@@ -2,7 +2,7 @@
 
 $errors = [];
 function validate($name, $email, $examDate, $grade, $subject) {
-    return validateName($name);
+    return validateName($name) & validateEmail($email) & validateExamDate($examDate) & validateGrade($grade) & validateSubject($subject);
 }
 
 function validateName($name) {
@@ -14,6 +14,61 @@ function validateName($name) {
         return false;
     } else if(strlen($name) > 20) {
         $errors['name'] = "Name zu lang";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateExamDate($examDate) {
+
+    global $errors;
+    try {
+        if (strlen($examDate) == "") {
+            $errors['examDate'] = "Prüfungsdatum darf nicht leer sein";
+            return false;
+        } else if (new DateTime($examDate) > new DateTime()) {
+            $errors['examDate'] = "Prüfungsdatum darf nicht in der Zukunft liegen";
+            return false;
+        } else {
+            return true;
+        }
+    } catch(Exception $e) {
+        $errors['examDate'] = "Prüfungsdatum ungültig";
+        return false;
+    }
+}
+
+function validateSubject($subject) {
+
+    global $errors;
+
+    if($subject != 'm' || $subject != 'e' || $subject != 'd') {
+        $errors['subject'] = "Fach ungültig";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateGrade($grade) {
+
+    global $errors;
+
+    if(!is_numeric($grade) || $grade < 1 || $grade > 5) {
+        $errors['grade'] = "Note ungültig";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateEmail($email) {
+
+    global $errors;
+
+    if($email != "" && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "E-Mail ungültig";
         return false;
     } else {
         return true;
