f00c857c77bf4a24780b6c9cba9e97014fe136e2
FSchnabel2 <frauschnabel623@gmail.com>
2025-10-10 10:35:13 +0200

    Grundlegende PHP Struktur aufbauen

--- 1_2_SalesPrediction/SalesPrediction/index.php
+++ 1_2_SalesPrediction/SalesPrediction/index.php
@@ -14,6 +14,52 @@
 
 <div class="container">
     <h1 class="mt-5 mb-3">Sales Prediction</h1>
+    <?php
+
+    require "lib/func.inc.php";
+
+    $tv = "";
+    $radio = "";
+    $newspaper = "";
+
+    ?>
+
+    <form id="form_salespred" method="POST" action="index.php">
+        <div class="row">
+            <div class="col-sm-12 form-group">
+                <label for="tv">TV</label>
+                <input type="text" name="tv" class="form-control <?= isset($errors['tv']) ? 'is-invalid' : '' ?>"
+                       value="<?= htmlspecialchars($tv) ?>"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 form-group">
+                <label for="radio">Radio</label>
+                <input type="text" name="radio" class="form-control <?= isset($errors['radio']) ? 'is-invalid' : '' ?>"
+                       value="<?= htmlspecialchars($radio) ?>"/>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12 form-group">
+                <label for="radio">Newspaper</label>
+                <input type="text" name="newspaper" class="form-control <?= isset($errors['newspaper']) ? 'is-invalid' : '' ?>"
+                       value="<?= htmlspecialchars($newspaper) ?>"/>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+
+            <div class="col-sm-3 mb-3">
+                <input type="submit" name="submit" class="btn btn-primary btn-block" value="Vorhersagen">
+            </div>
+
+            <div class="col-sm-3">
+                <a href="index.php" class="btn btn-secondary btn-block" role="button">Löschen</a>
+            </div>
+        </div>
+    </form>
 </div>
 
 </body>
