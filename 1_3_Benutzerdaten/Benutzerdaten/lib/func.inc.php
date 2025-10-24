9777e9989b380454dc16b595e70b129a7ecaee33
FSchnabel2 <frauschnabel623@gmail.com>
2025-10-24 09:13:46 +0200

    Suchfunktion fixen

--- 1_3_Benutzerdaten/Benutzerdaten/lib/func.inc.php
+++ 1_3_Benutzerdaten/Benutzerdaten/lib/func.inc.php
@@ -18,6 +18,7 @@ function getFilteredData($filter) {
             str_contains(strtolower($user["email"]), $filter)
         ) {
             $result[] = [
+                'id'        => $user["id"],
                 'firstname' => $user['firstname'],
                 'lastname'  => $user['lastname'],
                 'email'     => $user['email'],
